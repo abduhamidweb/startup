73a946516b48176070d1c2c9db37b6e30cdf3ffd
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-06-27 21:41:46 +0500

    ended

--- src/controller/technology.contr.js
+++ src/controller/technology.contr.js
@@ -1,7 +1,11 @@
 import Technologies from "../schemas/technology.schema.js";
 import Products from "../schemas/product.schema.js";
-import { JWT } from "../utils/jwt.js";
-import { User } from "../schemas/user.schema.js";
+import {
+  JWT
+} from "../utils/jwt.js";
+import {
+  User
+} from "../schemas/user.schema.js";
 
 const errorObj = (err) => {
   return {
@@ -15,12 +19,16 @@ class TechnologyController {
   constructor() {}
   static async Get(req, res) {
     try {
-      let { id } = req.params;
-      let { name } = req.query;
+      let {
+        id
+      } = req.params;
+      let {
+        name
+      } = req.query;
       if (id) {
         let data = await Technologies.findById(id);
-        if(data == null){
-           throw new Error(`${id} - technology Not Found!`)
+        if (data == null) {
+          throw new Error(`${id} - technology Not Found!`)
         }
         res.send({
           status: 200,
@@ -29,8 +37,10 @@ class TechnologyController {
           data: data,
         });
       } else if (name) {
-        let findByNameCat = await Technologies.findOne({ name  });
-        if(findByNameCat == null){
+        let findByNameCat = await Technologies.findOne({
+          name
+        });
+        if (findByNameCat == null) {
           throw new Error(`Qanday Yozilgan bo'lsa shunday filter qiling, katta yoki kichik harifiga ham e'tibor bering, to'liq yozing!`)
         }
         res.send({
@@ -40,11 +50,35 @@ class TechnologyController {
           data: findByNameCat,
         });
       } else {
+        let technology = await Technologies.find();
+        let products = await Products.find().populate("technology");
+        const technologyNames = {};
+
+        // Teknoloji adlarını ve sayılarını hesaplamak için ürünleri döngüleyin
+        products.forEach(product => {
+          product.technology.forEach(tech => {
+            const techName = tech.name;
+            if (technologyNames.hasOwnProperty(techName)) {
+              technologyNames[techName]++;
+            } else {
+              technologyNames[techName] = 1;
+            }
+          });
+        });
+
+        // Teknoloji dizisini döngüleyerek count özelliğini ekleyin
+        technology = technology.map(tech => {
+          const techName = tech.name;
+          tech.__v = technologyNames[techName] || 0;
+          return tech;
+        });
+
+        // console.log(technology);
         res.send({
           status: 200,
           message: "All Technologies",
           success: true,
-          data: await Technologies.find(),
+          data: technology
         });
       }
     } catch (error) {
@@ -54,14 +88,24 @@ class TechnologyController {
 
   static async Post(req, res) {
     try {
-      let { name, img_link } = req.body;
-      let { token } = req.headers;
-      let { id } = JWT.VERIFY(token);
+      let {
+        name,
+        img_link
+      } = req.body;
+      let {
+        token
+      } = req.headers;
+      let {
+        id
+      } = JWT.VERIFY(token);
       let checkAdmin = await User.findById(id);
       if (checkAdmin.role != "admin") {
         throw new Error("Only admin can add!");
       }
-      let newTechnology = await Technologies.create({ name, img_link });
+      let newTechnology = await Technologies.create({
+        name,
+        img_link
+      });
       if (!newTechnology) {
         throw new Error(`Not added technology!`);
       }
@@ -78,36 +122,43 @@ class TechnologyController {
 
   static async Put(req, res) {
     try {
-      let { name, img_link } = req.body;
-      let { token } = req.headers;
-      let { id } = JWT.VERIFY(token);
+      let {
+        name,
+        img_link
+      } = req.body;
+      let {
+        token
+      } = req.headers;
+      let {
+        id
+      } = JWT.VERIFY(token);
       let checkAdmin = await User.findById(id);
       if (checkAdmin.role != "admin") {
         throw new Error("Only admin can update!");
       }
-       let findByIdTechnology = await Technologies.findById(req.params.id);
-       if(findByIdTechnology == null){
+      let findByIdTechnology = await Technologies.findById(req.params.id);
+      if (findByIdTechnology == null) {
         throw new Error(`Not Found ${id} - technology`)
-       }
-      if(!name && !img_link){
+      }
+      if (!name && !img_link) {
         throw new Error(`Not Found Target!`)
       }
       let updatedTechnology = await Technologies.findByIdAndUpdate(
-        req.params.id,
-        {
+        req.params.id, {
           name,
           img_link,
-        },
-        {new : true}
+        }, {
+          new: true
+        }
       );
 
       console.log(updatedTechnology);
-    
+
       res.send({
         status: 200,
         message: `Updated ${id} - technology`,
         success: true,
-        data:updatedTechnology,
+        data: updatedTechnology,
       });
     } catch (error) {
       res.send(errorObj(error));
@@ -116,8 +167,12 @@ class TechnologyController {
 
   static async Delete(req, res) {
     try {
-      let { token } = req.headers;
-      let { id } = JWT.VERIFY(token);
+      let {
+        token
+      } = req.headers;
+      let {
+        id
+      } = JWT.VERIFY(token);
       let checkAdmin = await User.findById(id);
       if (checkAdmin.role != "admin") {
         throw new Error("Only admin can delete!");
